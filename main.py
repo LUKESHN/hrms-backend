e008174350b7f74ac0cf6c7c927950dbe0338568
Lukesh Nehete <www.lukesh121@gmail.com>
2026-02-10 10:38:14 +0530

    enable cors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from datetime import date
@@ -6,6 +7,14 @@ from database import SessionLocal, engine, Base
 import models, schemas
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Create tables AFTER app starts (Render safe)
 @app.on_event("startup")
